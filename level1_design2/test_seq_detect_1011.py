5605bdd2ea5622d47705c3b6c98e6056fbd7b6b3
VINISHA <vinisharam2410@gmail.com>
2022-07-31 14:28:54 +0000

    update3

--- level1_design2/test_seq_detect_1011.py
+++ level1_design2/test_seq_detect_1011.py
@@ -26,22 +26,61 @@ async def test_seq_bug1(dut):
 
     dut.inp_bit.value = 1
     await FallingEdge(dut.clk)
+    dut._log.info(f'inp={dut.inp_bit.value}  state={dut.current_state.value} out={dut.seq_seen.value}') 
     dut.inp_bit.value=0
     await FallingEdge(dut.clk)
+    dut._log.info(f'inp={dut.inp_bit.value}  state={dut.current_state.value} out={dut.seq_seen.value}') 
     dut.inp_bit.value=1
     await FallingEdge(dut.clk)
+    dut._log.info(f'inp={dut.inp_bit.value}  state={dut.current_state.value} out={dut.seq_seen.value}') 
     dut.inp_bit.value=1
     await FallingEdge(dut.clk)
+    dut._log.info(f'inp={dut.inp_bit.value}  state={dut.current_state.value} out={dut.seq_seen.value} ') 
     dut.inp_bit.value=0
     await FallingEdge(dut.clk)
+    dut._log.info(f'inp={dut.inp_bit.value}  state={dut.current_state.value} out={dut.seq_seen.value}') 
     errors=[]
     try:
             assert dut.seq_seen.value == 1, "error"
     except:
             errors.append(dut.seq_seen.value)
-            dut._log.info(f'ERROR input={dut.inp_bit.value} state={dut.current_state.value} out={dut.seq_seen.value} expected_output={1}')
+            dut._log.info(f'ERROR at input={dut.inp_bit.value} state={dut.current_state.value} out={dut.seq_seen.value} expected_output={1}')
+            dut._log.info(f'BUG_1: Output is not seen after one clock cycle instead it is seen at same clock cycle')
               
 
+    dut.reset.value = 1
+    await FallingEdge(dut.clk) 
+    dut._log.info(f' state={dut.current_state.value} inp={dut.inp_bit.value} out={dut.seq_seen.value} ') 
+    dut.reset.value = 0
+    await FallingEdge(dut.clk)
+    dut._log.info(f' state={dut.current_state.value} inp={dut.inp_bit.value} out={dut.seq_seen.value}')
+    cocotb.log.info('#### CTB: Develop your test here! ######')
+
+    dut.inp_bit.value = 1
+    await FallingEdge(dut.clk)
+    dut._log.info(f'inp={dut.inp_bit.value} state={dut.current_state.value} out={dut.seq_seen.value}')
+    dut.inp_bit.value = 1
+    await FallingEdge(dut.clk)
+    dut._log.info(f'inp={dut.inp_bit.value} state={dut.current_state.value}  out={dut.seq_seen.value}')
+    dut.inp_bit.value=0
+    await FallingEdge(dut.clk)
+    dut._log.info(f'inp={dut.inp_bit.value} state={dut.current_state.value} out={dut.seq_seen.value} ')
+    dut.inp_bit.value=1
+    await FallingEdge(dut.clk)
+    dut._log.info(f'inp={dut.inp_bit.value} state={dut.current_state.value} out={dut.seq_seen.value} ')
+    dut.inp_bit.value=1
+    await FallingEdge(dut.clk)
+    dut._log.info(f'inp={dut.inp_bit.value} state={dut.current_state.value} out={dut.seq_seen.value} ')
+   
+    try:
+            assert dut.seq_seen.value == 1, "error"
+    except:
+            errors.append(dut.seq_seen.value)
+            dut._log.info(f'ERROR at input={dut.inp_bit.value} state={dut.current_state.value} out={dut.seq_seen.value} expected_output={1}')
+            dut._log.info(f'BUG_2: Sequence is not at all detected even in same clock cycle. Bug in state transition')
+            
+    dut.inp_bit.value=0
+    await FallingEdge(dut.clk)
     dut.reset.value = 1
     await FallingEdge(dut.clk) 
     dut._log.info(f' state={dut.current_state.value} inp={dut.inp_bit.value} ') 
@@ -52,30 +91,30 @@ async def test_seq_bug1(dut):
 
     dut.inp_bit.value = 1
     await FallingEdge(dut.clk)
-    dut._log.info(f' state={dut.current_state.value} inp={dut.inp_bit.value} ')
-    dut.inp_bit.value = 1
+    dut._log.info(f'inp={dut.inp_bit.value} state={dut.current_state.value} out={dut.seq_seen.value} ')
+    dut.inp_bit.value = 0
     await FallingEdge(dut.clk)
-    dut._log.info(f' state={dut.current_state.value} inp={dut.inp_bit.value} ')
+    dut._log.info(f'inp={dut.inp_bit.value} state={dut.current_state.value} out={dut.seq_seen.value} ')
+    dut.inp_bit.value=1
+    await FallingEdge(dut.clk)
+    dut._log.info(f'inp={dut.inp_bit.value} state={dut.current_state.value} out={dut.seq_seen.value}')
     dut.inp_bit.value=0
     await FallingEdge(dut.clk)
-    dut._log.info(f' state={dut.current_state.value} inp={dut.inp_bit.value} ')
+    dut._log.info(f'inp={dut.inp_bit.value} state={dut.current_state.value} out={dut.seq_seen.value}')
     dut.inp_bit.value=1
     await FallingEdge(dut.clk)
-    dut._log.info(f' state={dut.current_state.value} inp={dut.inp_bit.value} ')
+    dut._log.info(f'inp={dut.inp_bit.value} state={dut.current_state.value} out={dut.seq_seen.value} ')
     dut.inp_bit.value=1
     await FallingEdge(dut.clk)
-    dut._log.info(f' state={dut.current_state.value} inp={dut.inp_bit.value} ')
-    dut.inp_bit.value=1
-    await FallingEdge(dut.clk)
-
+    dut._log.info(f'inp={dut.inp_bit.value} state={dut.current_state.value} out={dut.seq_seen.value}')
+   
     try:
             assert dut.seq_seen.value == 1, "error"
     except:
             errors.append(dut.seq_seen.value)
-            dut._log.info(f'ERROR input={dut.inp_bit.value} state={dut.current_state.value} out={dut.seq_seen.value} expected_output={1}')
+            dut._log.info(f'ERROR at input={dut.inp_bit.value} state={dut.current_state.value} out={dut.seq_seen.value} expected_output={1}')
+            dut._log.info(f'BUG_3 Sequence is not at all detected even in same clock cycle. Bug in state transition')
             
-
-    
     
 
     
